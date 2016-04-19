fbc5c85d2ed23548654f6e8c4d12e435d58c2a38
chavezfk <chavezfk@gmail.com>
2016-04-19 16:45:36 -0600

    worked on queue

---
+++ Hw8/src/com/fchavez/jashinhurst/hw8/one/Queue.java
@@ -0,0 +1,29 @@
+package com.fchavez.jashinhurst.hw8.one;
+
+import java.util.ArrayDeque;
+
+public class Queue<E> {
+	ArrayDeque<E> deque;
+	public Queue(){
+		this.deque = new ArrayDeque<E>();
+	}
+	public Queue(int size){
+		this.deque = new ArrayDeque<E>(size);
+	}
+	
+	public void enque(E element){
+		this.deque.push(element);
+	}
+	public void deque(){
+		this.deque.poll();
+	}
+	public boolean isEmpty(){
+		return this.deque.isEmpty();
+	}
+	public E peek(){
+		return this.deque.peek();
+	}
+	public int size(){
+		return this.deque.size();
+	}
+}
