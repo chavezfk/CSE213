3884bd405765e6acfabe5273965c472adf4e8322
Joshua Ashinhurst <joshuaashinhurst@Joshuas-MacBook-Pro.local>
2016-04-19 21:22:04 -0600

    changed ArrayList in DFS

--- Hw8/src/com/fchavez/jashinhurst/hw8/one/GraphTest.java
+++ Hw8/src/com/fchavez/jashinhurst/hw8/one/GraphTest.java
@@ -14,26 +14,26 @@ public class GraphTest {
 		G.addVertex("H");
 
 		// print out graph
-		//System.out.println(G);
+		System.out.println(G);
 
 		
 		
 		Queue<Vertex> ret;
 		Vertex u;
-		ret = SearchDFS.DFS(G);
-		System.out.println("DFS");
-		while(!ret.isEmpty()){
-			System.out.println(ret.deque());
+		ret = SearchBFS.BFS(G,G.getVertex("A"));
+		System.out.println("BFS");
+		while(ret.deque.size()>0){
+			printf()
 		}
 		// print out graph again by iterating over vertices and edges
 		//this is equivalent to System.out.println(G);
-		/*for (Vertex v : G.getVertices()) {
+		for (Vertex v : G.getVertices()) {
 			System.out.print(v + ": ");
 			for (Vertex w : G.adjacentTo(v.getName())) {
 				System.out.print(w + " ");
 			}
 			System.out.println();
-		}*/
+		}
 		
 
 	}
