0f9b4b9807f2bc018ea730b6a51de981e5fb1d7a
Joshua Ashinhurst <joshuaashinhurst@Joshuas-MacBook-Pro.local>
2016-04-19 18:38:06 -0600

    completed DFS

--- Hw8/src/com/fchavez/jashinhurst/hw8/one/SearchDFS.java
+++ Hw8/src/com/fchavez/jashinhurst/hw8/one/SearchDFS.java
@@ -1,5 +1,30 @@
 package com.fchavez.jashinhurst.hw8.one;
 
 public class SearchDFS {
-
+	public static void DFS(Graph G) {
+		for (Vertex v : G.getVertices()) {
+			v.color = Color.WHITE;
+			v.p = null;
+		}
+		
+		for (Vertex v: G.getVertices()) {
+			if (v.color == Color.WHITE) {
+				dfsVisit(G, v);
+			}
+		}
+	}
+	
+	public static void dfsVisit(Graph G, Vertex v) {
+		v.color = Color.GRAY;
+		for (Vertex u: G.adjacentTo(v)) {
+			if (u.color == Color.WHITE) {
+				u.p = v;
+				dfsVisit(G,u);
+			}
+		}
+		v.color = Color.BLACK;
+		System.out.println("Visited " + v.getName());
+	}
+	
+	
 }
